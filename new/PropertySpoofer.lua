478bde53536acaa18eb4c39f45174d42ade8dee6
engo <63791277+joeengo@users.noreply.github.com>
2023-04-16 02:06:33 +0100

    Added unload functionality, patched metatable __concat detection and other detections

--- new/PropertySpoofer.lua
+++ new/PropertySpoofer.lua
@@ -5,6 +5,17 @@ local oldIndex, oldNewIndex
 local propertySpoofingFunctions = {}
 local updateValueFunctions = {}
 
+-- Unload functionality:
+local unload = getgenv().SPOOF_PROP_UNLOAD_DATA
+if (unload) then 
+    hookmetamethod(game, "__index", unload.oldIndex)
+    hookmetamethod(game, "__newindex", unload.oldNewIndex)
+    propertySpoofingFunctions = unload.propertySpoofingFunctions
+    updateValueFunctions = unload.updateValueFunctions
+    getgenv().SPOOF_PROP_UNLOAD_DATA = nil
+    unload = nil
+end
+
 local get_thread_identity = (syn and syn.get_thread_identity) or getthreadidentity
 local set_thread_identity = (syn and syn.set_thread_identity) or setthreadidentity
 
@@ -17,13 +28,40 @@ local function getDebugId(inst)
     return debugId
 end
 
+local function rawsetproperty(obj, idx, val, customFunc) 
+    local disabled = {}
+    for i, v in next, getconnections(obj.Changed) do 
+        table.insert(disabled, v)
+        v:Disable()
+    end
+    for i, v in next, getconnections(obj:GetPropertyChangedSignal(idx)) do 
+        table.insert(disabled, v)
+        
+        v:Disable()
+    end
+    if customFunc then
+        customFunc(obj, idx, val)
+    else
+        obj[idx] = val
+    end
+    for i, v in next, disabled do 
+        v:Enable()
+    end
+end
+
 oldIndex = hookmetamethod(game, "__index", function(self, index) 
+    local oldResult = oldIndex(self, index)
     if checkcaller() then 
-        return oldIndex(self, index)
+        return oldResult
     end
 
-    local typeOf = typeof(self)
-    if typeOf == "Instance" then -- Should always be true, but what do i know
+    local typeOfSelf = typeof(self)
+    local typeOfInd = typeof(index)
+    if typeOfInd ~= "string" then 
+        return oldResult
+    end
+
+    if typeOfSelf == "Instance" then -- Should always be true, but what do i know
         local className = oldIndex(self, "ClassName") -- self.ClassName
         local debugId = getDebugId(self)
 
@@ -36,34 +74,49 @@ oldIndex = hookmetamethod(game, "__index", function(self, index)
         end
     end
 
-    return oldIndex(self, index)
+    return oldResult
 end)
 
 oldNewIndex = hookmetamethod(game, "__newindex", function(self, index, value) 
-    if checkcaller() then 
+    local caller = checkcaller()
+    local typeOfSelf = typeof(self)
+    local typeOfInd = typeof(index)
+    if typeOfInd ~= "string" then 
         return oldNewIndex(self, index, value)
     end
 
-    local typeOf = typeof(self)
-    if typeOf == "Instance" then -- Should always be true, but what do i know
+    if typeOfSelf == "Instance" then -- Should always be true, but what do i know
         local className = self.ClassName
         local debugId = getDebugId(self)
 
         local cnFunc, idFunc = updateValueFunctions[className .. index], updateValueFunctions[debugId .. index]
-        if idFunc then 
-            idFunc(self, value)
+        if idFunc and not caller then 
+            idFunc(self, index, value)
         end 
-        if cnFunc then 
-            cnFunc(self, value)
+        if cnFunc and not caller then 
+            cnFunc(self, index, value)
         end 
         
         if cnFunc or idFunc then 
+            if caller then
+                rawsetproperty(self, index, value, oldNewIndex) -- Prevents game knowing that you set the property after using spoofprop
+            end
+
             return
         end
     end
 
     return oldNewIndex(self, index, value)
 end)
+
+-- This function basically sets the propertys value to the value and then returns it indexed from the instance, this removes quite a few detections.
+local function validateValueForObject(self, index, value) 
+    local oldValue = oldIndex(self, index)
+    rawsetproperty(self, index, value)
+    local realValue = oldIndex(self, index)
+    rawsetproperty(self, index, oldValue)
+    return realValue
+end
 
 -- toSpoof: a classname or instance, both should work
 -- property: name of property
@@ -72,25 +125,33 @@ local function SpoofProperty(toSpoof, property, value)
     local typeOf = typeof(toSpoof)
     if typeOf == "Instance" then 
         local debugId = getDebugId(toSpoof)
-        local newValue = (typeof(value) == "function" and value(toSpoof)) or value
-        propertySpoofingFunctions[debugId .. property] = function(self) 
+        local newValue = value
+        propertySpoofingFunctions[debugId .. property] = function(self)
             return newValue
         end
-        updateValueFunctions[debugId .. property] = function(self, val) 
-            newValue = val
+        updateValueFunctions[debugId .. property] = function(self, index, val)
+            local realVal = validateValueForObject(self, index, val)
+            newValue = realVal
         end
     elseif typeOf == "string" then -- its a classname
         local debugIdValueTable = {}
         propertySpoofingFunctions[toSpoof .. property] = function(self) 
             local debugId = getDebugId(self)
-            return debugIdValueTable[debugId] or (typeof(value) == "function" and value(toSpoof)) or value
+            return debugIdValueTable[debugId] or value
         end
-        updateValueFunctions[toSpoof .. property] = function(self, val) 
+        updateValueFunctions[toSpoof .. property] = function(self, index, val)
             local debugId = getDebugId(self)
-            debugIdValueTable[debugId] = val
+            local realVal = validateValueForObject(self, index, val)
+            debugIdValueTable[debugId] = realVal
         end
     end
 end
 
 getgenv().SpoofProperty = SpoofProperty
-return SpoofProperty
+
+getgenv().SPOOF_PROP_UNLOAD_DATA = {
+    oldNewIndex = oldNewIndex,
+    oldIndex = oldIndex,
+    propertySpoofingFunctions = propertySpoofingFunctions,
+    updateValueFunctions = updateValueFunctions,
+}
